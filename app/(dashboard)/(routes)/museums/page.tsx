0408e9dbbc598b7ab3f137083bb6b6c6a2537316
MEARZIM <asahaayan@gmail.com>
2024-11-01 12:46:19 +0530

    feat: add logout feature

--- app/(dashboard)/(routes)/museums/page.tsx
+++ app/(dashboard)/(routes)/museums/page.tsx
@@ -5,9 +5,17 @@ import {
   useState
 } from "react";
 import axios from "axios";
+import { LogOut } from "lucide-react";
+import { SignOutButton } from "@clerk/nextjs";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
-import { UserAvatar } from "@/components/layouts/User-Avatar/user-avatar";
+import { Button } from "@/components/ui/button";
 
 export const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
@@ -51,15 +59,15 @@ const MuseumListPage = () => {
   if (isLoading) {
     return (
       <>
-      <div className="m-auto">
-        <div className="grid grid-cols-3 gap-4 max-w-4xl mx-auto">
-          {Array(9)
-            .fill(0)
-            .map((_, index) => (
-              <Skeleton key={index} />
-            ))}
+        <div className="m-auto">
+          <div className="grid grid-cols-3 gap-4 max-w-4xl mx-auto">
+            {Array(9)
+              .fill(0)
+              .map((_, index) => (
+                <Skeleton key={index} />
+              ))}
+          </div>
         </div>
-      </div>
       </>
     );
   }
@@ -71,7 +79,28 @@ const MuseumListPage = () => {
         <div className="text-gray-200">
           This is the list of all the available museums.
         </div>
-        <UserAvatar/>
+        <div className="flex gap-2 items-center">
+          <SignOutButton>
+
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant={"outline"}
+                    size={"icon"}
+                    className="rounded-full text-white hover:text-black"
+                  >
+                    <LogOut className="h-4 w-4" />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Logout</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+          </SignOutButton>
+        </div>
       </div>
       <BentoGrid className="max-w-4xl mx-auto">
         {museums.length > 0 ? (
